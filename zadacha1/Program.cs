925b45c1bfb4ee22cc6e1c992576ee560ae20da2
Vasiliy <evtuth@gmail.com>
2023-02-17 11:35:38 +0300

    added Method collecting array elements by condition

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -6,3 +6,16 @@
 
 string[] array1 = new string[10] {"1", "12", "123", "1234", "12345","h", "he", "hel", "hell", "hello"};
 string[] array2 = new string[array1.Length];
+
+void CollectingArrayElementsByCondition(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
